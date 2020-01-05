d2364b86dc67d09eab7d7b02bb73e042c58d472b
Pavel Yudkin <pymati@gmail.com>
2020-01-05 11:26:09 +0300

    1 week finished

---
+++ div.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+d = a % b
+q = 0 ** d
+w = d ** q
+y = 'YES'
+n = 'NO'
+print(y * q, n * w)
--- max.py
+++ max.py
@@ -1,4 +1,7 @@
 a = int(input())
 b = int(input())
 p = a * b
-s = a + b 
+s = a + b
+n = a // b * (a - b)
+m = b // a
+print(a * (n ** m) + b * (m ** n))
