b57b0dbaf9f592093df5e11196a0d378df3e8023
Pavel Yudkin <pymati@gmail.com>
2020-02-21 23:15:11 +0300

    Final commit 7th week

--- 7 week/elections/elections.py
+++ 7 week/elections/elections.py
@@ -1,7 +1,27 @@
 inFile = open('input.txt', 'r', encoding='utf8')
 outFile = open('output.txt', 'w', encoding='utf8')
 
-inFile.readlines()
+lines = inFile.read().splitlines()
+
+candidates = dict()
+for i in range(len(lines)):
+    if lines[i] not in candidates:
+        candidates[lines[i]] = 0
+    candidates[lines[i]] += 1
+
+canList = list(candidates.items())
+canList = sorted(canList, key=lambda x: x[1], reverse=True)
+
+if int(canList[0][1]) * 2 > len(lines):
+    print(canList[0][0],
+          file=outFile)
+else:
+    print(canList[0][0],
+          canList[1][0],
+          sep='\n',
+          file=outFile)
+
+# print(lines, candidates, canList, sep='\n')
 
 inFile.close()
 outFile.close()
